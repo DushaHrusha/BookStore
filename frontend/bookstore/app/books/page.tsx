e1392aa74c974b24e92e5d84c458adb6d32c727e
Andey Nikitin <hrushadusha@gmail.com>
2024-09-09 21:59:01 +0600

    Create logic of book form

--- frontend/bookstore/app/books/page.tsx
+++ frontend/bookstore/app/books/page.tsx
@@ -1,17 +1,27 @@
 "use client";
 import Button from "antd/es/button/button"
 import { Books } from "../components/Book";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getAllBooks } from "../services/books";
+import Title from "antd/es/typography/Title";
+
 
 export default function BooksPage(){
+    const [books, setBooks] = useState<Book[]>([]);
+    const [loading, setLoading] = useState(true);
     useEffect(()=>{
+        const getBooks = async () => {
+            const books = await getAllBooks();
+            setLoading(false);
+            setBooks(books);
+        };
 
+        getBooks();
     }, [])
     return(
         <div>
             <Button>Добавить книгу</Button>
-            
-            <Books books={books}/>
+            {loading ? <Title>Loading....</Title> : <Books books={books}/>}
         </div>
     )
 }
--- frontend/bookstore/app/services/books.ts
+++ frontend/bookstore/app/services/books.ts
@@ -5,8 +5,34 @@ export interface BookRequest{
 }
 
 export const getAllBooks = async ()=>{
-    const response = await fetch("http://localhost:5079/Books")
+    const response = await fetch("http://localhost:5282/Books")
     return response.json();
-}
+};
 
-1 08
+
+export const updateBook = async (bookRequest: BookRequest)=>{
+    await fetch("http://localhost:5282/Books", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify(bookRequest),
+        
+    });
+};
+
+export const createBook = async (id: string, bookRequest: BookRequest)=>{
+    await fetch('http://localhost:5282/Books/${id}', {
+        method: "PUT",
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify(bookRequest),
+    });
+};
+
+export const deleteBook = async (id: string)=>{
+    await fetch('http://localhost:5282/Books/${id}', {
+        method: "DELETE",
+    });
+};
